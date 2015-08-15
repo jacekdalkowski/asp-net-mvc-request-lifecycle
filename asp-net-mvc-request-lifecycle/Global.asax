08503d772220856a678f54489ea4df7d55673754
Jacek Dalkowski <jacekdalkowski@gmail.com>
2015-08-15 07:31:33 -0700

    Initial project

---
+++ asp-net-mvc-request-lifecycle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp_net_mvc_request_lifecycle.MvcApplication" Language="C#" %>
---
+++ asp-net-mvc-request-lifecycle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp_net_mvc_request_lifecycle.Startup))]
+namespace asp_net_mvc_request_lifecycle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
